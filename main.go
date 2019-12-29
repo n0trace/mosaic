c83b393b5dc3ab8d0161923948fe56f95ee724a8
n0trace <n0trace@protonmail.com>
2019-12-29 21:39:57 +0800

    add recover

--- main.go
+++ main.go
@@ -1,22 +1,44 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
 	"runtime"
-	"io/ioutil"
 )
 
-func uploadHandlerFunc(w http.ResponseWriter, r *http.Request) {	
+func uploadHandlerFunc(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("upload.html")
 	t.Execute(w, len(TILESDB))
 }
 
 func fetchHandlerFunc(w http.ResponseWriter, r *http.Request) {
-	files,_ := ioutil.ReadDir("tiles")	
+	files, _ := ioutil.ReadDir("tiles")
 	t, _ := template.ParseFiles("fetch.html")
 	t.Execute(w, len(files))
+}
+
+func RecoverWrap(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var err error
+		defer func() {
+			r := recover()
+			if r != nil {
+				switch t := r.(type) {
+				case string:
+					err = errors.New(t)
+				case error:
+					err = t
+				default:
+					err = errors.New("Unknown error")
+				}
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		}()
+		h.ServeHTTP(w, r)
+	})
 }
 
 func main() {
@@ -26,13 +48,13 @@ func main() {
 	files := http.FileServer(http.Dir("public"))
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
 
-	mux.HandleFunc("/", uploadHandlerFunc)
-	mux.HandleFunc("/reload", reloadTilesDBHandlerFunc)
-	mux.HandleFunc("/fetch", fetchHandlerFunc)
-	mux.HandleFunc("/fetch_tiles", fetchTilesHandlerFunc)
-	mux.HandleFunc("/mosaic_no_concurrency", noConcurrencyHandlerFunc)
-	mux.HandleFunc("/mosaic_fanout_channel", fanOutWithChannelHandlerFunc)
-	mux.HandleFunc("/mosaic_fanout_fanin", fanOutFanInHandlerFunc)
+	mux.Handle("/", RecoverWrap(http.HandlerFunc(uploadHandlerFunc)))
+	mux.Handle("/reload", RecoverWrap(http.HandlerFunc(reloadTilesDBHandlerFunc)))
+	mux.Handle("/fetch", RecoverWrap(http.HandlerFunc(fetchHandlerFunc)))
+	mux.Handle("/fetch_tiles", RecoverWrap(http.HandlerFunc(fetchTilesHandlerFunc)))
+	mux.Handle("/mosaic_no_concurrency", RecoverWrap(http.HandlerFunc(noConcurrencyHandlerFunc)))
+	mux.Handle("/mosaic_fanout_channel", RecoverWrap(http.HandlerFunc(fanOutWithChannelHandlerFunc)))
+	mux.Handle("/mosaic_fanout_fanin", RecoverWrap(http.HandlerFunc(fanOutFanInHandlerFunc)))
 
 	server := &http.Server{
 		Addr:    "127.0.0.1:8080",
